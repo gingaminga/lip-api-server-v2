04ade94903df7b1d74416ee28874bfcc9fd9a702
gingaminga <cjhwaan@gmail.com>
2024-04-17 23:44:50 +0900

    ✅ Test: 할 일 추가 API 단위 / 통합 테스트

--- src/__tests__/Integration/api/to-do/add-to-do.api.test.ts
+++ src/__tests__/Integration/api/to-do/add-to-do.api.test.ts
@@ -1,8 +1,11 @@
 import app from "@app";
+import User from "@my-rdb/entities/user.entity";
 import { startRDB, stopRDB } from "@my-rdb/index";
 import { ToDoRepository } from "@my-rdb/repositories/to-do.repository";
+import { UserRepository } from "@my-rdb/repositories/user.repository";
 import { HTTP_STATUS_CODE, RESPONSE_STATUS } from "@utils/constants";
 import ERROR_MESSAGE from "@utils/error-message";
+import { createJWTToken } from "@utils/jwt";
 import request from "supertest";
 
 const path = "/api/to-do";
@@ -15,15 +18,52 @@ describe(`POST ${path} API test :)`, () => {
     date,
   };
 
+  let user: User;
+  let token = "";
+
   beforeEach(() => {
     params.content = content;
     params.date = date;
   });
 
+  beforeAll(async () => {
+    await startRDB();
+
+    const mockedUser: User = new User();
+    mockedUser.id = 1;
+    mockedUser.email = "test@test.com";
+    mockedUser.nickname = "test";
+    mockedUser.createdAt = new Date();
+    mockedUser.socialKey = "test-1234";
+    mockedUser.socialType = "kakao";
+
+    user = await UserRepository.save(mockedUser, {
+      transaction: false,
+    });
+
+    // access token 생성
+    const payload = {
+      nickname: user.nickname,
+    };
+    token = createJWTToken(payload, {
+      expiresIn: "1m",
+    });
+  });
+
+  afterAll(async () => {
+    await UserRepository.remove(user);
+
+    await stopRDB();
+  });
+
   describe(`${HTTP_STATUS_CODE.INVALID_VALUE} test :)`, () => {
     it(`should error when require parameter is not exist.`, async () => {
       // when
-      const { body, status } = await request(app).post(path);
+      const { body, status } = await request(app)
+        .post(path)
+        .set({
+          Authorization: `Bearer ${token}`,
+        });
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.INVALID_VALUE);
@@ -36,7 +76,12 @@ describe(`POST ${path} API test :)`, () => {
       params.date = "20240101";
 
       // when
-      const { body, status } = await request(app).post(path).send(params);
+      const { body, status } = await request(app)
+        .post(path)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send(params);
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.INVALID_VALUE);
@@ -49,7 +94,12 @@ describe(`POST ${path} API test :)`, () => {
       params.date = "1234-56-78";
 
       // when
-      const { body, status } = await request(app).post(path).send(params);
+      const { body, status } = await request(app)
+        .post(path)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send(params);
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.INVALID_VALUE);
@@ -58,50 +108,32 @@ describe(`POST ${path} API test :)`, () => {
     });
   });
 
-  describe(`${HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR} test :)`, () => {
-    it(`should respond error.`, async () => {
-      // when
-      const { body, status } = await request(app).post(path).send(params);
-
-      // then
-      expect(status).toBe(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR);
-      expect(body.data).toEqual({ message: ERROR_MESSAGE.INTERNAL_SERVER_ERROR });
-      expect(body.status).toEqual(RESPONSE_STATUS.FAILURE);
-    });
-  });
-
   describe(`${HTTP_STATUS_CODE.CREATED} test :)`, () => {
-    beforeAll(async () => {
-      await startRDB();
-    });
-
-    afterAll(async () => {
-      await stopRDB();
-    });
-
     it(`should respond with new to do info.`, async () => {
       // when
-      const { body, status } = await request(app).post(path).send(params);
+      const { body, status } = await request(app)
+        .post(path)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send(params);
       const todo = await ToDoRepository.findOne({
         where: {
-          id: body.data.id,
+          id: body.data.toDoInfo.id,
         },
       });
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.CREATED);
       expect(todo).not.toBeNull();
-      expect(body.data).toEqual(
+      expect(body.data.toDoInfo).toEqual(
         expect.objectContaining({
           ...todo,
-          createdAt: body.data.createdAt.toString(),
-          updatedAt: body.data.updatedAt.toString(),
+          createdAt: body.data.toDoInfo.createdAt.toString(),
+          updatedAt: body.data.toDoInfo.updatedAt.toString(),
         }),
       );
       expect(body.status).toEqual(RESPONSE_STATUS.SUCCESS);
-
-      const removeToDoPath = `/api/to-do/${body.data.id}`;
-      await request(app).delete(removeToDoPath);
     });
   });
 });
--- src/__tests__/unit/services/to-do/add.method.test.ts
+++ src/__tests__/unit/services/to-do/add.method.test.ts
@@ -1,4 +1,5 @@
 import ToDo from "@my-rdb/entities/to-do.entity";
+import User from "@my-rdb/entities/user.entity";
 import { ToDoRepository } from "@my-rdb/repositories/to-do.repository";
 import { ToDoService } from "@services/to-do.service";
 
@@ -9,6 +10,13 @@ describe(`[ToDo service] add method test :)`, () => {
   const toDoService = new ToDoService(mockedToDoRepository);
   const content = "test";
   const date = "20240101";
+  const mockedUser = new User();
+  mockedUser.id = 1;
+  mockedUser.email = "test@test.com";
+  mockedUser.nickname = "test";
+  mockedUser.createdAt = new Date();
+  mockedUser.socialKey = "test-1234";
+  mockedUser.socialType = "kakao";
 
   it(`should be error by save method of toDoRepository.`, async () => {
     // given
@@ -17,7 +25,7 @@ describe(`[ToDo service] add method test :)`, () => {
 
     // when & then
     expect(async () => {
-      await toDoService.add(content, date);
+      await toDoService.add(content, date, mockedUser);
     }).rejects.toThrow(error);
   });
 
@@ -26,12 +34,13 @@ describe(`[ToDo service] add method test :)`, () => {
     const toDo = new ToDo();
     toDo.content = content;
     toDo.date = date;
+    toDo.createdAt = new Date();
     mockedToDoRepository.save.mockResolvedValue(toDo);
 
     // when
-    const newToDo = await toDoService.add(content, date);
+    const newToDo = await toDoService.add(content, date, mockedUser);
 
     // then
-    expect(newToDo).toEqual(toDo);
+    expect(newToDo.toDoInfo).toEqual(toDo);
   });
 });

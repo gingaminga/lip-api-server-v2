b40e500cc7bb1bb3f436b27fe07ade63b4f3acc3
gingaminga <cjhwaan@gmail.com>
2024-04-24 00:15:52 +0900

    ✅ Test: 할 일 삭제 API 단위 / 통합 테스트

--- src/__tests__/Integration/api/to-do/remove-to-do.api.test.ts
+++ src/__tests__/Integration/api/to-do/remove-to-do.api.test.ts
@@ -1,14 +1,49 @@
 import app from "@app";
+import User from "@my-rdb/entities/user.entity";
 import { startRDB, stopRDB } from "@my-rdb/index";
+import { UserRepository } from "@my-rdb/repositories/user.repository";
 import { HTTP_STATUS_CODE, RESPONSE_STATUS } from "@utils/constants";
 import ERROR_MESSAGE from "@utils/error-message";
+import { createJWTToken } from "@utils/jwt";
 import request from "supertest";
 
 const path = "/api/to-do";
 
-describe(`POST ${path}/:id API test :)`, () => {
+describe(`DELETE ${path}/:id API test :)`, () => {
   const id = 1;
-  const fullPath = `${path}/${id}`;
+
+  let user: User;
+  let token = "";
+
+  beforeAll(async () => {
+    await startRDB();
+
+    const mockedUser: User = new User();
+    mockedUser.id = 1;
+    mockedUser.email = "test@test.com";
+    mockedUser.nickname = "test";
+    mockedUser.createdAt = new Date();
+    mockedUser.socialKey = "test-1234";
+    mockedUser.socialType = "kakao";
+
+    user = await UserRepository.save(mockedUser, {
+      transaction: false,
+    });
+
+    // access token 생성
+    const payload = {
+      nickname: user.nickname,
+    };
+    token = createJWTToken(payload, {
+      expiresIn: "1m",
+    });
+  });
+
+  afterAll(async () => {
+    await UserRepository.remove(user);
+
+    await stopRDB();
+  });
 
   describe(`${HTTP_STATUS_CODE.NOT_FOUND} test :)`, () => {
     it(`should error when require parameter is not exist.`, async () => {
@@ -28,7 +63,11 @@ describe(`POST ${path}/:id API test :)`, () => {
       const fakePath = `${path}/test`;
 
       // when
-      const { body, status } = await request(app).delete(fakePath);
+      const { body, status } = await request(app)
+        .delete(fakePath)
+        .set({
+          Authorization: `Bearer ${token}`,
+        });
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.INVALID_VALUE);
@@ -37,31 +76,15 @@ describe(`POST ${path}/:id API test :)`, () => {
     });
   });
 
-  describe(`${HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR} test :)`, () => {
-    it(`should respond error.`, async () => {
-      // when
-      const { body, status } = await request(app).delete(fullPath);
-
-      // then
-      expect(status).toBe(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR);
-      expect(body.data).toEqual({ message: ERROR_MESSAGE.INTERNAL_SERVER_ERROR });
-      expect(body.status).toEqual(RESPONSE_STATUS.FAILURE);
-    });
-  });
-
   describe(`${HTTP_STATUS_CODE.OK} test :)`, () => {
-    beforeAll(async () => {
-      await startRDB();
-    });
-
-    afterAll(async () => {
-      await stopRDB();
-    });
-
     it(`should reponse -1 in id because not exist to do.`, async () => {
       // when
       const fakePath = `${path}/${10000}`;
-      const { body, status } = await request(app).delete(fakePath);
+      const { body, status } = await request(app)
+        .delete(fakePath)
+        .set({
+          Authorization: `Bearer ${token}`,
+        });
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.OK);
@@ -76,14 +99,24 @@ describe(`POST ${path}/:id API test :)`, () => {
         content: "Test add content",
         date: "2024-01-01",
       };
-      await request(app).post(addToDoPath).send(addToDoParams);
+      const { body: addToDoBody } = await request(app)
+        .post(addToDoPath)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .send(addToDoParams);
+      const fullPath = `${path}/${addToDoBody.data.toDoInfo.id}`;
 
       // when
-      const { body, status } = await request(app).delete(fullPath);
+      const { body, status } = await request(app)
+        .delete(fullPath)
+        .set({
+          Authorization: `Bearer ${token}`,
+        });
 
       // then
       expect(status).toBe(HTTP_STATUS_CODE.OK);
-      expect(body.data.id).toBe(id);
+      expect(body.data.id).toBe(addToDoBody.data.toDoInfo.id);
       expect(body.status).toEqual(RESPONSE_STATUS.SUCCESS);
     });
   });
--- src/__tests__/unit/services/to-do/remove.method.test.ts
+++ src/__tests__/unit/services/to-do/remove.method.test.ts
@@ -1,4 +1,5 @@
-import RemoveToDoDTO from "@dto/remove-to-do.dto";
+import RemoveToDoResponseDTO from "@dto/responses/to-do/remove-to-do.response.dto";
+import User from "@my-rdb/entities/user.entity";
 import { ToDoRepository } from "@my-rdb/repositories/to-do.repository";
 import { ToDoService } from "@services/to-do.service";
 
@@ -8,6 +9,13 @@ const mockedToDoRepository = jest.mocked(ToDoRepository);
 describe(`[ToDo service] remove method test :)`, () => {
   const toDoService = new ToDoService(mockedToDoRepository);
   const id = 1;
+  const mockedUser = new User();
+  mockedUser.id = 1;
+  mockedUser.email = "test@test.com";
+  mockedUser.nickname = "test";
+  mockedUser.createdAt = new Date();
+  mockedUser.socialKey = "test-1234";
+  mockedUser.socialType = "kakao";
 
   it(`should be error by modifyContent method of toDoRepository.`, async () => {
     // given
@@ -16,17 +24,17 @@ describe(`[ToDo service] remove method test :)`, () => {
 
     // when & then
     expect(async () => {
-      await toDoService.remove(id);
+      await toDoService.remove(id, mockedUser);
     }).rejects.toThrow(error);
   });
 
   it(`should be return todo array.`, async () => {
     // given
-    const dto = new RemoveToDoDTO(id);
+    const dto = new RemoveToDoResponseDTO(id);
     mockedToDoRepository.remove.mockResolvedValue(true);
 
     // when
-    const result = await toDoService.remove(id);
+    const result = await toDoService.remove(id, mockedUser);
 
     // then
     expect(result).toEqual(dto);
